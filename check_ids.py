b3c3924ce535c34fc724d4063355ab3dacf92140
look a look <1024726590@qq.com>
2026-07-20 16:38:03 +0800

    Add check_ids.py

---
+++ check_ids.py
@@ -0,0 +1,12 @@
+﻿import re
+
+with open(r'project/templates/index.html', 'r', encoding='utf-8') as f:
+    html = f.read()
+
+# Find ALL getElementById references
+for m in re.findall(r'getElementById\(['\x60"]([^'\x60"]*)['\x60"]\)', html):
+    print(m)
+print('---')
+# Find all id=\" attributes in HTML elements
+for m in re.findall(r'\bid=\"([a-zA-Z0-9_]+)\"', html):
+    print(m)
